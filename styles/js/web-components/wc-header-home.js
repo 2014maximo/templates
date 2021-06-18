8270bba8c3b9da3d1a73a88f9549f41ed5788c62
almunoz <almunoz@cotrafa.com.co>
2021-06-18 00:55:18 -0500

    Integrando web-components

---
+++ styles/js/web-components/wc-header-home.js
@@ -0,0 +1,40 @@
+class SellHeaderHome extends HTMLElement {
+      constructor () {
+        super();
+      }
+      
+      connectedCallback () {
+        this.innerHTML = `
+        <div class="bg-dark header fixed-top text-primary pt-3 pb-3 grandes transparenciaUno">
+            <div class="row">
+                <div class="col-1"></div>
+                <div class="col-4">
+                    <h2 class="tituloDos text-light">TRUCOS PA' DESARROLLO</h2>
+                </div>
+                <div class="col-6">
+                    <form class="formUno">
+                        <input class="inputUno text-light fuente bg-transparent" type="search" placeholder="Buscar" aria-label="Search">
+                    </form>
+                </div>
+                <div class="col-1"></div>
+            </div>
+        </div>
+        <div class="pos-f-t mobile fixed-top">
+            <div class="collapse" id="navbarToggleExternalContent">
+            <div class="bg-dark p-4">
+                <form class="formUno tituloDos">
+                    <input class="inputUno text-light bg-transparent" type="search" placeholder="Buscar" aria-label="Search">
+                </form>
+            </div>
+            </div>
+            <nav class="navbar navbar-dark bg-dark text-light">
+                <h2 class="fuenteDos">TRUCOS PA' DESARROLLO</h2>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            </nav>
+        </div>
+       `;
+      }
+    }
+window.customElements.define('sell-head-home', SellHeaderHome)
