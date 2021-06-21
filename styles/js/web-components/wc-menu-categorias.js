18056b9a7f35775c0f08fadde17131577e78530a
almunoz <almunoz@cotrafa.com.co>
2021-06-21 07:30:03 -0500

    rediseño home

--- styles/js/web-components/wc-menu-categorias.js
+++ styles/js/web-components/wc-menu-categorias.js
@@ -9,71 +9,66 @@ class SellMenuCategorias extends HTMLElement {
       <div class="col-md-1"></div>
       <div class="col-md-10">
     
-        <div class="row">
-          <div class="col-md-8">
-            <div class="menu-categorias p-5">
-    
-              <div class="d-flex justify-content-between mb-5 text-center row">
-                <div class="linkCategoria col" onClick="menuUno('angular', '80%')">
+        <div class="row flex-lg-row-reverse">
+          <div class="col-lg-4">
+            <h2 class="text-light categorias">CATEGORIAS</h2>
+          </div>
+          <div class="col-lg-8" data-aos="flip-left">
+              <div class="p-4 d-flex justify-content-around align-items-center text-center drop" style="background-image:url('./styles/assets/categorias/tablaMenu1.png');background-repeat:no-repeat;background-size:cover;">
+                <div class="linkCategoria" onClick="menuUno('angular', '80%')">
                   <img src="styles/assets/categorias/angular.png" alt="" width="auto" height="80" class="drop">
                 </div>
-                <div class="linkCategoria col" onClick="menuUno('html', '80%')">
+                <div class="linkCategoria" onClick="menuUno('html', '80%')">
                   <img src="styles/assets/categorias/html.png" alt="" width="auto" height="80" class="drop">
                 </div>
-                <div class="linkCategoria col" onClick="menuUno('css', '80%')">
+                <div class="linkCategoria" onClick="menuUno('css', '80%')">
                   <img src="styles/assets/categorias/css.png" alt="" width="auto" height="80" class="drop">
                 </div>
-                <div class="linkCategoria col" onClick="menuUno('javascript', '80%')">
+                <div class="linkCategoria" onClick="menuUno('javascript', '80%')">
                   <img src="styles/assets/categorias/javascript.png" alt="" width="auto" height="80" class="drop">
                 </div>
               </div>
     
-              <div class="d-flex justify-content-around mb-5 text-center row">
-                <div class="linkCategoria col" onClick="menuUno('php', '80%')">
+              <div class="p-4 d-flex justify-content-around align-items-center text-center drop" style="background-image: url('./styles/assets/categorias/tablaMenu1.png');background-repeat: no-repeat;background-size: cover;">
+                <div class="linkCategoria" onClick="menuUno('php', '80%')">
                   <img src="styles/assets/categorias/php.png" alt="" width="auto" height="80" class="drop">
                 </div>
-                <div class="linkCategoria col" onClick="menuUno('wordpress', '80%')">
+                <div class="linkCategoria" onClick="menuUno('wordpress', '80%')">
                   <img src="styles/assets/categorias/wordpress2.png" alt="" width="auto" height="80" class="drop">
                 </div>
-                <div class="linkCategoria col" onClick="menuUno('linuxmint', '80%')">
+                <div class="linkCategoria" onClick="menuUno('linuxmint', '80%')">
                   <img src="styles/assets/categorias/linuxmint.png" alt="" width="auto" height="80" class="drop">
                 </div>
               </div>
     
-              <div class="d-flex justify-content-around mb-5 text-center row">
-                <div class="linkCategoria col" onClick="menuUno('debian', '80%')">
+              <div class="p-4 d-flex justify-content-around align-items-center text-center drop" style="background-image:url('./styles/assets/categorias/tablaMenu1.png');background-repeat:no-repeat;background-size:cover;">
+                <div class="linkCategoria" onClick="menuUno('debian', '80%')">
                   <img src="styles/assets/categorias/debian.png" alt="" width="auto" height="80" class="drop">
                 </div>
-                <div class="linkCategoria col" onClick="menuUno('visual_studio_code', '80%')">
-                  <img src="styles/assets/categorias/visual_studio_code_text_white.png" alt="" width="auto" height="100" class="drop">
+                <div class="linkCategoria" onClick="menuUno('visual_studio_code', '80%')">
+                  <img src="styles/assets/categorias/visual_studio_code_text_white.png" alt="" width="auto" height="80" class="drop">
                 </div>
-                <div class="linkCategoria col" onClick="menuUno('flutter', '80%')">
-                  <img src="styles/assets/categorias/flutter_text_white.png" alt="" width="auto" height="100" class="drop">
+                <div class="linkCategoria" onClick="menuUno('flutter', '80%')">
+                  <img src="styles/assets/categorias/flutter_text_white.png" alt="" width="auto" height="80" class="drop">
                 </div>
-                <div class="linkCategoria col" onClick="menuUno('dart', '80%')">
+                <div class="linkCategoria" onClick="menuUno('dart', '80%')">
                   <img src="styles/assets/categorias/dart_text_white.png" alt="" width="auto" height="40" class="drop">
                 </div>
               </div>
     
-              <div class="d-flex justify-content-around mb-5 text-center row">
-                <div class="linkCategoria col" onClick="menuUno('develop', '80%')">
-                  <img src="styles/assets/categorias/devlop.png" alt="" width="auto" height="40" class="drop">
+              <div class="p-1 d-flex justify-content-around align-items-center text-center drop" style="background-image:url('./styles/assets/categorias/tablaMenu1.png');background-repeat:no-repeat;background-size:cover;">
+                <div class="linkCategoria" onClick="menuUno('develop', '80%')">
+                  <img src="styles/assets/categorias/devlop.png" alt="" width="auto" height="60" class="drop">
                 </div>
-                <div class="linkCategoria col" onClick="menuUno('git', '80%')">
-                  <img src="styles/assets/categorias/git-light.png" alt="" width="auto" height="40" class="drop">
+                <div class="linkCategoria" onClick="menuUno('git', '80%')">
+                  <img src="styles/assets/categorias/git-light.png" alt="" width="auto" height="60" class="drop">
                 </div>
-                <div class="linkCategoria col" onClick="menuUno('java', '80%')">
-                  <img src="styles/assets/categorias/logo-java.png" alt="" width="auto" height="120" class="drop">
+                <div class="linkCategoria" onClick="menuUno('java', '80%')">
+                  <img src="styles/assets/categorias/logo-java.png" alt="" width="auto" height="115" class="drop">
                 </div>
               </div>
-    
-            </div>
-          </div>
-          <div class="col-md-4">
-            <h2 class="text-light categorias">CATEGORIAS</h2>
           </div>
         </div>
-    
       </div>
       <div class="col-md-1"></div>
     </div>
