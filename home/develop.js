9d96bdf68fdf8a9e9cac88eb1a0e9c3f3c665674
Alex Muñoz <alexuriel.777@gmail.com>
2021-07-03 16:28:47 -0500

    cambio nombre de archivo

--- home/develop.js
+++ home/develop.js
@@ -15,12 +15,12 @@ class SellMenuCatDevelop extends HTMLElement {
               <img src="styles/assets/categorias/devlop.png" alt="" width="auto" height="40" class="drop">
             </div>
             <div class="row m-3">
-              <div class="col bg-Dart pos-z l-menu"><a href="componentes/DEVELOP/develop_001.html">SUPER RECURSOS</a></div>
-              <div class="col bg-Dart rot-2 l-menu"><a href="componentes/DEVELOP/develop_001.html">PSEINT EL INICIO</a></div>
-              <div class="col bg-Dart rot-1 l-menu"><a href="componentes/DEVELOP/develop_001.html">VISUAL STUDIO CODE</a></div>
-              <div class="col bg-Dart rot-2 l-menu"><a href="componentes/DEVELOP/develop_001.html">SUBLIME TEXT</a></div>
-              <div class="col bg-Dart pos-z l-menu"><a href="componentes/DEVELOP/develop_001.html">ATOM</a></div>
-              <div class="col bg-Dart pos-z l-menu"><a href="componentes/DEVELOP/develop_001.html">ALGORITMOS AVANZADOS</a></div>
+              <div class="col bg-Develop pos-z l-menu"><a href="componentes/DEVELOP/dev_001.html">SUPER RECURSOS</a></div>
+              <div class="col bg-Develop rot-2 l-menu"><a href="componentes/DEVELOP/develop_001.html">PSEINT EL INICIO</a></div>
+              <div class="col bg-Develop rot-1 l-menu"><a href="componentes/DEVELOP/develop_001.html">VISUAL STUDIO CODE</a></div>
+              <div class="col bg-Develop rot-2 l-menu"><a href="componentes/DEVELOP/develop_001.html">SUBLIME TEXT</a></div>
+              <div class="col bg-Develop pos-z l-menu"><a href="componentes/DEVELOP/develop_001.html">ATOM</a></div>
+              <div class="col bg-Develop pos-z l-menu"><a href="componentes/DEVELOP/develop_001.html">ALGORITMOS AVANZADOS</a></div>
             </div>
           </div>
           <div class="col-md-1"></div>
