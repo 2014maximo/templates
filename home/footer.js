301c9a184fd9d418ac79fbc6921f0f1e32f25203
2014maximo <alexuriel.777@gmail.com>
2021-07-07 00:45:51 -0500

    terminando footer

--- home/footer.js
+++ home/footer.js
@@ -5,7 +5,28 @@ class SellMenuFooter extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-
+        <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-10">
+                <div class="row">
+                    <div class="col-md-6 d-flex justify-content-center align-items-center">
+                        <div class="text-center">
+                            <h2 class="tituloDos text-light sombra-texto">TRUCOS PA' DESARROLLO</h2>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="text-center">
+                            <div class="form-group fuenteDos">
+                                <input type="email" class="form-control" id="email" placeholder="Email">
+                                <textarea class="form-control" id="texarea" rows="3" placeholder="Comentarios"></textarea>
+                                <button type="submit" id="bt-enviar" class="p-2 btn-block text-light bg-warning sombraUno">Enviar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-1"></div>
+        </div>
     `;
     }
 }
