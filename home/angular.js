eda98b2de9c393f28982fbd5b2eb60b514d03cfd
almunoz <almunoz@cotrafa.com.co>
2021-07-09 13:52:22 -0500

    acorte nombres

--- home/angular.js
+++ home/angular.js
@@ -17,16 +17,17 @@ class SellMenuCatAngular extends HTMLElement {
             </a>
           </div>
           <div class="row m-3">
-            <div class="col bg-Angular pos-z l-menu"><a href="componentes/ANGULAR/angular_001.html">INSTALACIÓN</a></div>
-            <div class="col bg-Angular rot-2 l-menu"><a href="componentes/ANGULAR/angular_002.html">PRUEBAS UNITARIAS</a></div>
-            <div class="col bg-Angular rot-1 l-menu"><a href="componentes/ANGULAR/angular_003.html">INPUT Y OUTPUT</a></div>
-            <div class="col bg-Angular rot-2 l-menu"><a href="componentes/ANGULAR/angular_004.html">ELEMENTOS DESCRIPCIÓN...</a></div>
-            <div class="col bg-Angular pos-z l-menu"><a href="componentes/ANGULAR/angular_005.html">CRUD CON UN MOCKDATA</a></div>
-            <div class="col bg-Angular pos-z l-menu"><a href="componentes/ANGULAR/angular_006.html">RXJS</a></div>
-            <div class="col bg-Angular pos-z l-menu"><a href="componentes/ANGULAR/angular_007.html">MICROFRONTENDS</a></div>
-            <div class="col bg-Angular pos-z l-menu"><a href="componentes/ANGULAR/angular_008.html">MONTAR EN DOMINIO</a></div>
-            <div class="col bg-Angular rot-1 l-menu"><a href="componentes/ANGULAR/angular_009.html">REDUX</a></div>
-            <div class="col bg-Angular rot-2 l-menu"><a href="componentes/ANGULAR/angular_010.html">IONIC INSTALACIÓN</a></div>
+            <div class="col bg-Angular pos-z l-menu"><a href="componentes/ANGULAR/ang_001.html">INSTALACIÓN</a></div>
+            <div class="col bg-Angular rot-2 l-menu"><a href="componentes/ANGULAR/ang_002.html">PRUEBAS UNITARIAS</a></div>
+            <div class="col bg-Angular rot-1 l-menu"><a href="componentes/ANGULAR/ang_003.html">INPUT Y OUTPUT</a></div>
+            <div class="col bg-Angular rot-2 l-menu"><a href="componentes/ANGULAR/ang_004.html">ELEMENTOS DESCRIPCIÓN...</a></div>
+            <div class="col bg-Angular pos-z l-menu"><a href="componentes/ANGULAR/ang_005.html">CRUD CON UN MOCKDATA</a></div>
+            <div class="col bg-Angular pos-z l-menu"><a href="componentes/ANGULAR/ang_006.html">RXJS</a></div>
+            <div class="col bg-Angular pos-z l-menu"><a href="componentes/ANGULAR/ang_007.html">MICROFRONTENDS</a></div>
+            <div class="col bg-Angular pos-z l-menu"><a href="componentes/ANGULAR/ang_008.html">MONTAR EN DOMINIO</a></div>
+            <div class="col bg-Angular rot-1 l-menu"><a href="componentes/ANGULAR/ang_009.html">REDUX</a></div>
+            <div class="col bg-Angular rot-2 l-menu"><a href="componentes/ANGULAR/ang_010.html">IONIC INSTALACIÓN</a></div>
+            <div class="col bg-Angular rot-2 l-menu"><a href="componentes/ANGULAR/ang_011.html">TIEMPO REAL SOCKET Y REST</a></div>
           </div>
         </div>
         <div class="col-md-1"></div>
