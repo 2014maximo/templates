2fff5174e5a54036f0427d9f2661fed0d2ac2881
almunoz <almunoz@cotrafa.com.co>
2021-08-05 01:12:33 -0500

    actulizacion tablero

--- home/sql.js
+++ home/sql.js
@@ -18,15 +18,9 @@ class SellMenuCatSQL extends HTMLElement {
                 </div>
                 <div class="row m-3">
                     <div class="col bg-Sql pos-z l-menu"><a href="componentes/SQL/sql_001.html">MODELO RELACIONAL Y ENTIDAD RELACIÓN</a></div>
-                    <div class="col bg-Sql rot-2 l-menu"><a href="componentes/SQL/sql_002.html">DESCRIPCIÓN Y TÉRMINOS</a></div>
-                    <div class="col bg-Sql rot-1 l-menu"><a href="componentes/SQL/sql_003.html">DIFERENCIAS DE COMANDOS SQL</a></div>
-                    <div class="col bg-Sql rot-2 l-menu"><a href="componentes/SQL/sql_004.html">ELEMENTOS DESCRIPCIÓN...</a></div>
-                    <div class="col bg-Sql pos-z l-menu"><a href="componentes/SQL/sql_005.html">CRUD CON UN MOCKDATA</a></div>
-                    <div class="col bg-Sql pos-z l-menu"><a href="componentes/SQL/sql_006.html">RXJS</a></div>
-                    <div class="col bg-Sql pos-z l-menu"><a href="componentes/SQL/sql_007.html">MICROFRONTENDS</a></div>
-                    <div class="col bg-Sql pos-z l-menu"><a href="componentes/SQL/sql_008.html">MONTAR EN DOMINIO</a></div>
-                    <div class="col bg-Sql rot-1 l-menu"><a href="componentes/SQL/sql_009.html">REDUX</a></div>
-                    <div class="col bg-Sql rot-2 l-menu"><a href="componentes/SQL/sql_010.html">IONIC INSTALACIÓN</a></div>
+                    <div class="col bg-Sql rot-2 l-menu"><a href="componentes/SQL/sql_002.html">ELEMENTOS Y DESCRIPCIÓN</a></div>
+                    <div class="col bg-Sql rot-1 l-menu"><a href="componentes/SQL/sql_003.html">CONSULTAS</a></div>
+                    <div class="col bg-Sql rot-2 l-menu"><a href="componentes/SQL/sql_004.html">INSTALACIÓN MOTORES</a></div>
                 </div>
             </div>
             <div class="col-md-1"></div>
