17735c74012452a20ee6989a69c42b2c0bd76088
almunoz <almunoz@cotrafa.com.co>
2021-08-05 01:26:58 -0500

    adapación scripts

--- styles/js/scripts.js
+++ styles/js/scripts.js
@@ -32,7 +32,9 @@ function copiarAlPortapapeles(id_elemento) {
 
 const menuBtn = document.querySelector(".menu-btn");
 let menuOpen = false;
-menuBtn.addEventListener("click", () => {
+
+if(menuBtn){
+  menuBtn.addEventListener("click", () => {
   if (!menuOpen) {
     menuBtn.classList.add("open");
     menuOpen = true;
@@ -40,4 +42,5 @@ menuBtn.addEventListener("click", () => {
     menuBtn.classList.remove("open");
     menuOpen = false;
   }
-});
+  });
+}
