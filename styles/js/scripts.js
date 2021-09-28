6f75f0e5e36ac25450678016957241993bd5aa79
almunoz <almunoz@cotrafa.com.co>
2021-09-28 10:46:13 -0500

    primera propuesta azure

--- styles/js/scripts.js
+++ styles/js/scripts.js
@@ -45,3 +45,61 @@ if(menuBtn){
   }
   });
 }
+
+
+ function typing(){
+
+  var listaTexto = [
+                  "Ha llegado un nuevo integrante a nuestra familia,", 
+                  "se trata de AZURE",
+                  "-",
+                  "Es el momento de entender como trabajaremos con su compañía y le saquemos todo el provecho",
+                  "Los días presenciales estaremos en capacitaciones de:",
+                  "HISTORIAS DE USUARIO",
+                  "TABLEROS AZURE",
+                  "PIPELINES DE AZURE",
+                  "-",
+                  "-",
+                  "....",
+                  "¡CONTAMOS CONTIGO!",
+                ]  
+  maquinaLista("typer",80,listaTexto,1);
+  
+  function maquinaLista(contenedor, intervalo, listaTexto, indice){      
+	if (indice == listaTexto.length){
+        indice = 0;
+    }
+     maquina2(contenedor, listaTexto[indice], intervalo, listaTexto, indice);	
+  }
+  
+  function maquina2(contenedor, texto, intervalo, listaTexto, indiceLista){
+  var indiceTexto = 0;
+  var finalTexto = false;
+  timer = setInterval( function(){			  
+	if (indiceTexto == texto.length && finalTexto == false){
+		finalTexto = true;			
+	}
+			
+	if (finalTexto == false) indiceTexto++
+	else indiceTexto--;
+	mostrarEliminarTexto(contenedor, texto, indiceTexto, finalTexto);
+	if (finalTexto == true && indiceTexto == 0){
+		clearInterval(timer);
+		maquinaLista(contenedor, intervalo, listaTexto, indiceLista+1);
+	}		
+  },intervalo)
+  }	 
+  
+  function mostrarEliminarTexto(contenedor, texto, i, finalTexto){
+
+if (finalTexto){
+	$("#"+contenedor).html(texto.substr(0,i--) + "_");
+}
+else{
+	$("#"+contenedor).html(texto.substr(0,i++) + "_");
+}
+  }
+  
+  
+  
+};
